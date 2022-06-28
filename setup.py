265f9f2e1e1ab03a37bb290b3fb63aca54e4d8e8
assemer <102959350+assemer@users.noreply.github.com>
2022-06-28 12:41:34 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AmazightSymbole',
+      version='1.0',
+      description='',
+      author='Assemer LAYAS',
+      author_email='',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['turtle', 'tkinter']
+     )
